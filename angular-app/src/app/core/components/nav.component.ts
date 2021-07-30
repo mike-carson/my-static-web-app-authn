afc4f5efcb8fe5671d52a0960e5ff87aee20c6f5
Mike Carson <mike.carson@98ventures.com>
2021-07-30 15:50:15 -0400

    Added authentication

--- angular-app/src/app/core/components/nav.component.ts
+++ angular-app/src/app/core/components/nav.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserInfo } from '../model/user-info';
 
 @Component({
   selector: 'app-nav',
   template: `
+    <div class="user" *ngIf="userInfo">
+      <p>Welcome</p>
+      <p>{{ userInfo?.userDetails }}</p>
+      <p>{{ userInfo?.identityProvider }}</p>
+      <p *ngFor="let role of userInfo?.userRoles">{{ role }}</p>
+      <hr>
+    </div>
     <nav class="menu">
       <p class="menu-label">Menu</p>
       <ul class="menu-list">
@@ -14,6 +22,40 @@ import { Component } from '@angular/core';
         </a>
       </ul>
     </nav>
+    <nav class="menu auth">
+      <p class="menu-label">Auth</p>
+      <ul class="menu-list auth">
+        <ng-container *ngIf="!userInfo; else logout">
+          <ng-container *ngFor="let provider of providers">
+            <a href="/.auth/login/{{provider.value}}?post_login_redirect_uri={{redirect}}">{{provider.label}}</a>
+          </ng-container>
+        </ng-container>
+        <ng-template #logout>
+          <a href="/.auth/logout?post_logout_redirect_uri={{redirect}}">Logout</a>
+        </ng-template>
+      </ul>
+    </nav>
   `,
 })
-export class NavComponent {}
+export class NavComponent implements OnInit {
+  providers: any[] = [{value:"github",label:"GitHub"}, {value:"aad",label:"Active Directory"}];
+  redirect: string = window.location.pathname;
+  userInfo: UserInfo;
+
+  async ngOnInit() {
+    this.userInfo = await this.getUserInfo();
+  }
+
+  async getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      console.log(payload);
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch(error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
+}
